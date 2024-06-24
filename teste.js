ef214f113d72bdfdc69ce85fd33b333d2fe95ed1
Zephyr <171523581+gentlezephyr@users.noreply.github.com>
2024-06-24 15:28:08 -0300

    Criado parte da lógica

--- teste.js
+++ teste.js
@@ -1,5 +1,43 @@
-let word = "gato";
+function encriptarTexto() {
+    let palavraRecebida = document.querySelector('#campoTexto');
+    let saidaTexto = document.querySelector('.saidaTexto');
+    let novaPalavra = palavraRecebida.value;
 
-word = word.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    novaPalavra = novaPalavra
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
 
-console.log(word);
+    saidaTexto.innerHTML = novaPalavra;
+};
+
+function copiarTexto() {
+    let saidaTexto = document.querySelector('.saidaTexto');
+  
+    let range = document.createRange();
+    range.selectNode(saidaTexto);
+  
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+  
+    document.execCommand('copy');
+  
+    window.getSelection().removeAllRanges();
+};
+
+function desencriptarTexto() {
+    let palavraRecebida = document.querySelector('#campoTexto');
+    let saidaTexto = document.querySelector('.saidaTexto');
+    let novaPalavra = palavraRecebida.value;
+
+    novaPalavra = novaPalavra
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+
+    saidaTexto.innerHTML = novaPalavra;
+};
